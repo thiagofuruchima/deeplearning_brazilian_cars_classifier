b83c99e10235a32ce53cb96bb4b645bb56683bd4
thiagofuruchima <thiago.furuchima@gmail.com>
2021-03-21 17:49:58 -0300

    Removed unused library: matplotlib

--- app/model.py
+++ app/model.py
@@ -1,7 +1,6 @@
 import tensorflow as tf
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import io
 from PIL import Image
 import base64
@@ -74,53 +73,6 @@ def make_mock_predictions(uploaded_file):
     top_classes = [class_names.iloc[x]['car_model'] for x in label]
     
     prob = np.array(prob)
-    prob = np.round(prob*100,2)
+    prob = np.round(prob * 100, 2)
     
     return prob, top_classes
-
-def get_classification_plot(uploaded_file, top_k=5):
-    """ Use the model to predict the top N classes
-        for the in image specified by image_path 
-        and plot the image and the probabilities """
-    
-    # Retrieve car labels
-    class_names = pd.read_csv(current_app.config['BRAZILIAN_CAR_CLASSIFIER_DESC'])
-    
-    # Render the image
-    im = Image.open(uploaded_file)
-    image = np.asarray(im)    
-    
-    # use the model to predict class probabilities and label
-    #prob, label = make_predictions(uploaded_file, top_k)
-    
-    prob, label = [0.31024748, 0.22182074, 0.20753783, 0.14737591, 0.04855004], [70,  52, 36, 102, 61]
-   
-    current_app.logger.info(prob)
-    current_app.logger.info(label)
-
-    # use the predicted probabilities and labels to get class names
-    top_classes = [class_names.iloc[x]['car_model'] for x in label]
-    
-    current_app.logger.info(top_classes)
-
-    # plot each image and a probability barchart for the top classes
-    fig, (ax1, ax2) = plt.subplots(figsize=(9,12), nrows=2)
-    ax1.imshow(image, cmap = plt.cm.binary)
-    ax1.axis('off')
-    #ax1.set_title(class_names.iloc[label[0]])
-    ax2.barh(top_classes, prob)
-    ax2.set_aspect(0.1)
-    ax2.set_yticks(np.arange(len(label)))
-    current_app.logger.info("4")
-    ax2.set_yticklabels(top_classes, size='small');
-    ax2.set_title('Class Probability')
-    ax2.set_xlim(0, 1.1)
-    #plt.tight_layout()
-    
-    current_app.logger.info("5")
-    
-    data = io.BytesIO()
-    fig.savefig(data)    
-    encoded_img = base64.b64encode(data.getvalue())
-    
-    return encoded_img
